a9da1489269b744be2d488d88c85ce7cbd483c14
Abdulrauf_Remi <anadeenara@gmail.com>
2024-05-17 20:07:36 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -10,3 +10,24 @@ function processArray(numArray) {
 
 const numArray = [1,2,3,4,5,6,7,8,9,10];
 console.log(processArray(numArray));
+
+
+
+
+function formatArrayStrings(strArr, numArr) {
+    let formattedArr = [];
+    for (let i = 0; i < strArr.length; i++) {
+        if (numArr[i] % 2 === 0) {
+            formattedArr.push(strArr[i].toUpperCase());
+        } else {
+            formattedArr.push(strArr[i].toLowerCase());
+        }
+    }
+    return formattedArr;
+}
+
+// Example usage:
+let strArr = ["Remi", "Adeleke", "Mango", "University", "Legon", "America"];
+let numArr = [1, 2, 3, 4, 5, 6, 7, 8, 6, 4];
+
+console.log(formatArrayStrings(strArr, numArr));
